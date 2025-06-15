594fd9b09a03514df84f5bf01b1d684244a1f30d
Rohit kr Gupta <rohitkrgupta8292@gmail.com>
2025-06-15 17:35:06 +0530

    Update urls.py

--- final_project/final_project/urls.py
+++ final_project/final_project/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls",)),
     path('tinymce/', include(tinymce_urls)),  # Include TinyMCE URLs
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:  # Only serve media files in development
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:  # Only serve media files in development
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
